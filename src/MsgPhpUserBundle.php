05e4fb3ead143a11e81bcd4a689fe0c2b263f722
Roland Franssen <franssen.roland@gmail.com>
2017-10-28 15:01:49 +0200

    fixes

--- src/MsgPhpUserBundle.php
+++ src/MsgPhpUserBundle.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace MsgPhp\UserBundle;
 
+use MsgPhp\UserBundle\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 

6f3ac084059eaeffd62dc9fa9cb302761c84e855
Roland Franssen <franssen.roland@gmail.com>
2019-02-07 19:48:10 +0100

    removed file headers

--- Resources/skeleton/security.php
+++ Resources/skeleton/security.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the MsgPHP package.
- *
- * (c) Roland Franssen <franssen.roland@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 return <<<YAML
 # see https://github.com/symfony/recipes/blob/master/symfony/security-bundle/3.3/config/packages/security.yaml
 security:

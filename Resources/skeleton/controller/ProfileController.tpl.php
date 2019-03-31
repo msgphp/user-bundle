ae1fbbf26a9978b56f3adb8563e09402735f12ef
Roland Franssen <franssen.roland@gmail.com>
2019-03-31 15:24:27 +0200

    Generate default param converters (#321)

--- Resources/skeleton/controller/ProfileController.tpl.php
+++ Resources/skeleton/controller/ProfileController.tpl.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace <?= $controller_ns ?>;
 
+use <?= $user_class ?>;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -13,7 +15,10 @@ use Twig\Environment;
  */
 final class ProfileController
 {
-    public function __invoke(Environment $twig): Response
+    /**
+     * @ParamConverter("user", converter="msgphp.current_user")
+     */
+    public function __invoke(<?= $user_short_class ?> $user, Environment $twig): Response
     {
         return new Response($twig->render('<?= $template_dir ?>profile.html.twig'));
     }
--- Resources/skeleton/controller/ResetPasswordController.tpl.php
+++ Resources/skeleton/controller/ResetPasswordController.tpl.php
@@ -7,13 +7,12 @@ namespace <?= $controller_ns ?>;
 use <?= $user_class ?>;
 use <?= $form_ns ?>\ResetPasswordType;
 use MsgPhp\User\Command\ResetUserPassword;
-use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -23,21 +22,17 @@ use Twig\Environment;
  */
 final class ResetPasswordController
 {
+    /**
+     * @ParamConverter("user", converter="doctrine.orm", options={"mapping": {"token": "passwordResetToken"}})
+     */
     public function __invoke(
-        string $token,
+        <?= $user_short_class ?> $user,
         Request $request,
         FormFactoryInterface $formFactory,
         FlashBagInterface $flashBag,
         Environment $twig,
-        MessageBusInterface $bus,
-        EntityManagerInterface $em
+        MessageBusInterface $bus
     ): Response {
-        $user = $em->getRepository(<?= $user_short_class ?>::class)->findOneBy(['passwordResetToken' => $token]);
-
-        if (!$user instanceof <?= $user_short_class ?>) {
-            throw new NotFoundHttpException();
-        }
-
         $form = $formFactory->createNamed('', ResetPasswordType::class);
         $form->handleRequest($request);
 

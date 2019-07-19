9b0c5efe708d67229f5a277b5ab64e0b0c12abb4
Roland Franssen <franssen.roland@gmail.com>
2019-07-19 11:56:43 +0200

    update messenger config

--- Resources/skeleton/messenger.tpl.php
+++ Resources/skeleton/messenger.tpl.php
@@ -1,13 +1,18 @@
-# see https://github.com/symfony/recipes/blob/master/symfony/messenger/4.1/config/packages/messenger.yaml
+# see https://github.com/symfony/recipes/blob/master/symfony/messenger/4.3/config/packages/messenger.yaml
 framework:
     messenger:
+        # Uncomment this (and the failed transport below) to send failed messages to this transport for later handling.
+        # failure_transport: failed
+
         transports:
-            # Uncomment the following line to enable a transport named "amqp"
-            # amqp: '%env(MESSENGER_TRANSPORT_DSN)%'
+            # https://symfony.com/doc/current/messenger.html#transport-configuration
+            # async: '%env(MESSENGER_TRANSPORT_DSN)%'
+            # failed: 'doctrine://default?queue_name=failed'
+            # sync: 'sync://'
 
         routing:
             # Route your messages to the transports
-            # 'App\Message\YourMessage': amqp
+            # 'App\Message\YourMessage': async
 
         default_bus: command_bus
         buses:
